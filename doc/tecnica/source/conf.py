e7d1fb09c08e5ac496f2401760123366d3877cd7
Roberto Barison <roberto.barison@anche.no>
2012-10-08 12:45:04 +0200

    Documentazione compilabile e codice eseguibile

--- doc/tecnica/source/conf.py
+++ doc/tecnica/source/conf.py
@@ -90,7 +90,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'cedial'
+html_theme = 'tema_cedial'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -98,7 +98,7 @@ html_theme = 'cedial'
 html_theme_options = {'collapsiblesidebar': 'true',}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['../../../../sphinx/themes']
+html_theme_path = ['../..']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -119,7 +119,7 @@ html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
